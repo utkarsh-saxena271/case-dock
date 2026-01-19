31f76aa01df8a8423a9f391fb5e33c9eed72ccba
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2026-01-19 22:32:45 +0530

    fixed db config

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -8,6 +8,7 @@ const connectDB = async() => {
             throw new Error("Mongo URI is missing")
         }
         await mongoose.connect(MONGO_URI)
+        console.log("Connected to database")
     } catch (error) {
         console.error(error)
     }
