74020b9f6b51c1bf6e2905634b40c0afb352f60b
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2026-01-19 22:32:57 +0530

    added user model

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    fullName:{
+        firstName:{
+            type:String,
+            required:true
+        },
+        lastName:{
+            type:String,
+            required:true
+        }
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    enrollmentNumber:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+},{timestamps:true})
+
+
+const userModel = mongoose.model("User",userSchema)
+
+export default userModel;
