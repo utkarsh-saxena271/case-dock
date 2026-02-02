8a728430a6904d9fa1c1e76856a9a90d4935fa58
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2026-02-02 13:46:50 +0530

    added case routes to app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import connectDB from './db/db.js'
 import authRoutes from './routes/auth.routes.js'
+import caseRoutes from './routes/case.routes.js'
 
 
 // App
@@ -18,6 +19,7 @@ app.use(cookieParser())
 
 // Routes
 app.use('/api/auth',authRoutes)
+app.use('/api/cases', caseRoutes)
 
 
 
