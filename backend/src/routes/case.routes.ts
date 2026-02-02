59af328891958a6ea41f2268faa2be9a813cdc78
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2026-02-02 13:46:27 +0530

    updated case routes

--- backend/src/routes/case.routes.ts
+++ backend/src/routes/case.routes.ts
@@ -4,10 +4,10 @@ import { upload } from '../middlewares/multer.middleware.js';
 import { createCase, getAllCases, getCaseById, updateCase } from '../controllers/case.controller.js';
 const router = express.Router();
 
-router.get('/cases', authMiddleware, getAllCases)
-router.get('/cases/:caseId', authMiddleware, getCaseById)
-router.post('/case', authMiddleware, upload.array("files", 10), createCase)
-router.patch("/case/:caseId",authMiddleware,upload.array("files", 10),updateCase);
+router.get('/', authMiddleware, getAllCases)
+router.get('/:caseId', authMiddleware, getCaseById)
+router.post('/', authMiddleware, upload.array("files", 10), createCase)
+router.patch("/:caseId",authMiddleware,upload.array("files", 10),updateCase);
 
 
 
