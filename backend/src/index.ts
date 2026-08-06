e5186695afd0e904cbb35d58fb0cc2039a660b2a
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2026-08-06 18:31:41 +0530

    added main routes in entry file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import helmet from 'helmet'
+import cookieParser from 'cookie-parser'
 
 import { envConfig } from './config/env.config.js'
 import errorHandler from './middlewares/error.middleware.js';
-import authRouter from './routes/auth.route.js';
+import mainRouter from './routes/main.route.js';
+import client from './config/redis.config.js';
 
 const PORT = envConfig.PORT
 const app = express()
@@ -11,11 +13,12 @@ const app = express()
 
 // global middlewares
 app.use(express.json());
+app.use(cookieParser())
 app.use(helmet())
 
 
 // routes
-app.use('/api/auth', authRouter)
+app.use('/api', mainRouter)
 
 
 
@@ -24,10 +27,17 @@ app.use(errorHandler);
 
 
 // entry pointw
-const startServer = () => {
-    app.listen(PORT, () => {
-        console.log(`Server is listening at PORT : ${PORT}`)
-    })
+const startServer = async() => {
+    try {
+        await client.connect()
+
+        app.listen(PORT, () => {
+            console.log(`Server is listening at PORT : ${PORT}`)
+        })
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
 }
 
 startServer();
