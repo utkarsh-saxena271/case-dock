81d927b78010be086f03b2e3ea6eab275a9724d1
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2026-08-06 18:29:59 +0530

    added env configs

--- backend/src/config/env.config.ts
+++ backend/src/config/env.config.ts
@@ -11,5 +11,12 @@ export const envConfig = {
     NODE_ENV: requireEnv("NODE_ENV"),
     JWT_SECRET:requireEnv("JWT_SECRET"),
     DATABASE_URL:requireEnv("DATABASE_URL"),
-    REDIS_URL:requireEnv('REDIS_URL')
+    REDIS_URL:requireEnv('REDIS_URL'),
+    MAIL_HOST:requireEnv('MAIL_HOST'),
+    MAIL_USER:requireEnv('MAIL_USER'),
+    MAIL_PASS:requireEnv('MAIL_PASS'),
+    EMAIL_VERIFY_SECRET:requireEnv('EMAIL_VERIFY_SECRET'),
+    CLIENT_URL:requireEnv('CLIENT_URL'),
+    ACCESS_TOKEN_SECRET:requireEnv('ACCESS_TOKEN_SECRET'),
+    REFRESH_TOKEN_SECRET:requireEnv('REFRESH_TOKEN_SECRET')
 }
