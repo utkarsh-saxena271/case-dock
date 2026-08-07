5740ca5ca72c319dbf7aebc6b8f6d6a283558df6
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2026-08-07 15:13:59 +0530

    added chamber and membership router in the main router

--- backend/src/routes/main.route.ts
+++ backend/src/routes/main.route.ts
@@ -1,9 +1,13 @@
 import express from 'express'
 
 import authRouter from './auth.route.js'
+import chamberRouter from './chamber.route.js'
+import membershipRouter from './membership.route.js'
 
 const mainRouter = express.Router()
 
 mainRouter.use('/auth', authRouter)
+mainRouter.use('/chamber', chamberRouter)
+mainRouter.use('/chamber', membershipRouter)
 
 export default mainRouter
