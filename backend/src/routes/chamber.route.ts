947cfe05737bdb21d4909f7f66b840b44caa1562
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2026-08-07 15:13:28 +0530

    added chamber routes

---
+++ backend/src/routes/chamber.route.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import authMiddleware from '../middlewares/auth.middleware.js';
+import validate from '../middlewares/validate.middleware.js';
+import { createChamberSchema, updateChamberSchema } from '../validators/chamber.validator.js';
+import { createChamberController, deleteChamberController, discoverChambersController, getChamberByIdController, getMyChambersController, updateChamberController } from '../controllers/chamber.controller.js';
+import permissionMiddleware from '../middlewares/permission.middleware.js';
+import { requireOwnerMiddleware } from '../middlewares/requireOwner.middleware.js';
+import { requireMemberMiddleware } from '../middlewares/requireMember.middleware.js';
+
+const chamberRouter = express.Router();
+
+chamberRouter.post('/', authMiddleware, validate(createChamberSchema), createChamberController)
+chamberRouter.get('/', authMiddleware, getMyChambersController)
+chamberRouter.get('/discover', authMiddleware, discoverChambersController)
+chamberRouter.get('/:chamberId', authMiddleware, requireMemberMiddleware, getChamberByIdController)
+chamberRouter.patch('/:chamberId', authMiddleware, permissionMiddleware('EDIT_GROUP'), validate(updateChamberSchema), updateChamberController)
+chamberRouter.delete('/:chamberId', authMiddleware, requireOwnerMiddleware, deleteChamberController)
+
+export default chamberRouter;
