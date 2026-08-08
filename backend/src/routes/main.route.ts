3164448bd172098b93f5a4d22b79015b51b792a7
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2026-08-08 17:33:21 +0530

    added document, hearing and case routers in main router

--- backend/src/routes/main.route.ts
+++ backend/src/routes/main.route.ts
@@ -3,11 +3,17 @@ import express from 'express'
 import authRouter from './auth.route.js'
 import chamberRouter from './chamber.route.js'
 import membershipRouter from './membership.route.js'
+import caseRouter from './case.route.js'
+import hearingRouter from './hearing.route.js'
+import documentRouter from './document.route.js'
 
 const mainRouter = express.Router()
 
 mainRouter.use('/auth', authRouter)
 mainRouter.use('/chamber', chamberRouter)
 mainRouter.use('/chamber', membershipRouter)
+mainRouter.use('/cases', caseRouter)
+mainRouter.use('/cases', hearingRouter)
+mainRouter.use('/cases', documentRouter)
 
 export default mainRouter
