7aa98e1b2722b45fd9eec9082052e6b41b17304c
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2026-08-09 14:04:47 +0530

    added app and page, and all the config files

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import MainRoutes from './routes/mainRoutes'
+
+function App() {
+
+
+  return (
+    <div>
+      <MainRoutes/>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/main.tsx
@@ -0,0 +1,17 @@
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import App from './App.tsx'
+import { store } from './store/store.ts'
+import AuthInitializer from './components/AuthInitializer.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <AuthInitializer>
+        <App />
+      </AuthInitializer>
+    </BrowserRouter>
+  </Provider>
+)
