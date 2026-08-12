cc05073ffd3ca6609f98f9addc6567b182bd8c25
utkarsh-saxena271 <utkarshsaxena271@gmail.com>
2026-08-12 12:23:05 +0530

    added chamber routes inside protected routes

--- frontend/src/routes/MainRoutes.tsx
+++ frontend/src/routes/MainRoutes.tsx
@@ -6,11 +6,14 @@ import ForgotPassword from '../pages/auth/ForgotPassword'
 import ResetPassword from '../pages/auth/ResetPassword'
 import AuthLayout from '../pages/auth/AuthLayout'
 import ProtectedRoute from './ProtectedRoute'
+import ChamberList from '../pages/chambers/ChamberList'
+import CreateChamber from '../pages/chambers/CreateChamber'
+import ChamberDetails from '../pages/chambers/ChamberDetails'
 
 const MainRoutes = () => {
   return (
     <Routes>
-      <Route path='/auth' element={<AuthLayout/>}>
+      <Route path='/auth' element={<AuthLayout />}>
         <Route path='login' element={<Login />} />
         <Route path='register' element={<Register />} />
         <Route path='verify-email' element={<VerifyEmail />} />
@@ -19,8 +22,10 @@ const MainRoutes = () => {
       </Route>
 
       {/* Protected Routes */}
-      <Route element={<ProtectedRoute/>}>
-
+      <Route element={<ProtectedRoute />}>
+        <Route path="/chamber" element={<ChamberList />} />
+        <Route path="/chamber/create" element={<CreateChamber />} />
+        <Route path="/chamber/:chamberId" element={<ChamberDetails />} />
       </Route>
 
     </Routes>
